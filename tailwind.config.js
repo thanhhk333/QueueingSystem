436503c66dfcf418f859336e86922cb1ef293d98
thanhnguyen <thanhhk333@gmail.com>
2023-07-09 22:18:49 +0700

    refactory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit',
   theme: {
     extend: {}
   },
